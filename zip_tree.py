43f7da2f51bd9cbd95d5a43978966103946dd38e
Justin Chen <jchen698@ucr.edu>
2024-03-20 01:16:48 -0700

    Just made a new branch for recursive since I made one for iterative

--- zip_tree.py
+++ zip_tree.py
@@ -34,81 +34,89 @@ class ZipTree:
 		if rank == -1:
 			rank = self.get_random_rank()
 
-		# Generate a new node with the random rank
-		new_node = self.Node(key, val, rank)
+		# Generate a node x with x.key = k
+		x = self.Node(key, val, rank)
 
-		# Make sure it is not an empty tree
+		# Make sure it is not an empty tree, if it is, set node x to the root
 		if self.root is None:
-			self.root = new_node
+			self.root = x
 			self.size += 1
+			self.print_tree(self.root)
+			print("\n")
 			return
 		
 		# Search for the node with key 'K' in the tree
 
 		parent = None # Parent of the new_node
-		current = self.root # Essentially our 'y'
-		while current:
-			# If meets conditions, then break as current is where it should be as well as parent (unless it's the root)
-			if current.rank < rank or (current.rank == rank and current.key > key):
+		y = self.root # Current node which will eventually be our y node
+
+		# while we have not traversed the tree
+		while y:
+			# This node is y if y.rank < x.rank or (y.rank = x.rank and y.key > x.key)
+			if y.rank < x.rank or (y.rank == x.rank and y.key > x.key):
 				break
 
-			# If y has not been found, continue traversing the tree
-			parent = current
-			if key < current.key:
-				current = current.left
+			# If y has not been found, continue traversing the tree by BST rules
+			parent = y
+			if key < y.key:
+				y = y.left 
 			else:
-				current = current.right
+				y = y.right
 			
 		# If y is never found, then set as leaf node
-		if current is None:
+		if y is None:
 			if key < parent.key:
-				parent.left = new_node
+				parent.left = x
 			else:
-				parent.right = new_node
+				parent.right = x
 			self.size += 1
-			# self.print_tree(self.root)
-			# print("\n")
+			self.print_tree(self.root)
+			print("\n")
 			return
 
-		# Create paths P and Q by unzipping from node y
-		P, Q = self.unzip(current, new_node)
+		# Unzip the remainder of the search path for x (the portion starting with node y) to produce 2 paths, P and Q
+		P, Q = self.unzip(y, x)
 		
 		# Replace y with x and attach P and Q accordingly
 		if parent is None: # Parent is none, then new_node is at the root
-			new_node.left = P
-			new_node.right = Q
-			self.root = new_node
-		elif rank < parent.rank or (rank == parent.rank and key < parent.key):
-			new_node.left = P
-			new_node.right = Q
-			if parent.left == current:
-				parent.left = new_node
-			else:
-				parent.right = new_node
+			x.left = P # Make the top node of P the left child of x
+			x.right = Q # Make the top node of Q the right child of x
+			self.root = x # Node x replaces y as the root
+		else:
+			x.left = P # Make the top node of P the left child of x
+			x.right = Q # Make the top node of Q the right child of x
+			if parent.left == y: # Replace the left child of the parent of y
+				parent.left = x
+			else: # Replace the right child of the parent of y
+				parent.right = x
 		
 		self.size += 1
 
-		# self.print_tree(self.root)
-		# print("\n")
+		self.print_tree(self.root)
+		print("\n")
 
 	def unzip(self, y, x):
 		def unzip_lookup(k, node):
+			# if current node is None, reached a null ptr in tree
 			if node is None:
 				return (None, None)
-			if node.key < k:
-				P, Q = unzip_lookup(k, node.right)
-				node.right = P
-				return (node, Q)
-			else:
-				P, Q = unzip_lookup(k, node.left)
-				node.left = Q
-				return (P, node)
+			if node.key < k: # if node's key < k, node should be part of left output tree (P)
+				P, Q = unzip_lookup(k, node.right) # recursive call to process right subtree of node
+				node.right = P # node.right updated to P, subtrees with keys less than k
+				return (node, Q) # returns node as part of left subtree and Q
+			else: # if node.key > k, node belongs to right output tree (Q)
+				P, Q = unzip_lookup(k, node.left) # recursive call to process left subtree
+				node.left = Q # node.left updated to Q, subtree > k
+				return (P, node) # returns P as the left tree and current node as part of Q
 				
 		return unzip_lookup(x.key, y)
 
 	def remove(self, key: KeyType):
 		self.root = self.remove_recursive(self.root, key)
 		self.size -= 1
+
+		self.print_tree(self.root)
+		print("\n")
 
 	def remove_recursive(self, node, key):
 
@@ -144,17 +152,19 @@ class ZipTree:
 		return self.find_recursive(self.root, key)
 	
 	def find_recursive(self, node, key):
-		
+
 		# The node is not in the tree
 		if node is None:
 			return None
-		
+
 		if key == node.key:
 			return node.val # Found the node, return its value
 		elif key < node.key:
 			return self.find_recursive(node.left, key) # Traverse the left branch
 		else:
 			return self.find_recursive(node.right, key) # Traverse the right branch
+		
+		
 
 	def get_size(self) -> int:
 		return self.size
@@ -194,7 +204,7 @@ class ZipTree:
 		else:
 			return self.get_depth_recursive(node.right, key, depth + 1)
 		
-	'''
+
 	def print_tree(self, root, level = 0):
 		if root is None:
 			print("  " * level + "None")
@@ -202,7 +212,7 @@ class ZipTree:
 		print("  " * level + f"({root.key}, {root.val}, {root.rank})")
 		self.print_tree(root.left, level + 1)
 		self.print_tree(root.right, level + 1)
-	'''
+
 
 # feel free to define new classes/methods in addition to the above
 # fill in the definitions of each required member function (above),
